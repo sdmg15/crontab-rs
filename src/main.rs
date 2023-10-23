4378f53154ecae7a15825bc64d4826e8e2ece979
Sonkeng Maldini <sdmg15@pm.me>
2023-10-23 08:32:06 +0100

    missing main file update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use crontab::{CronEntry, validate};
-use std::str::FromStr;
+mod types;
+mod logic;
+
+use logic::CronEntry;
 use std::env;
+use std::str::FromStr;
 
-pub fn main() -> Result<(), Box<dyn std::error::Error>>{
-
+pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = env::args().collect::<Vec<String>>();
     let expr = &args[1];
     let s = CronEntry::from_str(expr).unwrap_or_else(|e| {
@@ -11,31 +13,6 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>>{
         std::process::exit(1);
     });
 
-    match validate(&s.minutes) {
-        false => println!("Invalid value provided for minutes"),
-        true => ()
-    }
-
-    match validate(&s.hour) {
-        false => println!("Invalid value provided for hour"),
-        true => ()
-    }
-
-    match validate(&s.month) {
-        false => println!("Invalid value provided for month"),
-        true => ()
-    }
-
-    match validate(&s.day_of_month) {
-        false => println!("Invalid value provided for day of month"),
-        true => ()
-    }
-
-    match validate(&s.day_of_week) {
-        false => println!("Invalid value provided for day of week"),
-        true => ()
-    }
-    
     println!("The cron expression [{}] is valid", expr);
     Ok(())
 }
